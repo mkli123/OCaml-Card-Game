6322322c6994ddfb426831497061837404247507
Michael Kuan <mmk248@cornell.edu>
2015-11-12 03:05:32 -0500

    here is board and deck mli

---
+++ board.mli
@@ -0,0 +1,55 @@
+open deck
+open hero
+open ai
+open draft
+open command
+
+(* keep track of current mana available to player at a given time
+ * max is the mana you start with next turn
+ *)
+type mana  = {
+	max     : int ref;
+	current : int ref;
+}
+
+(* mode defines whether or not it is player vs player or player vs ai *)
+type mode   = |VSai |PVP
+
+(* board keeps track of all the different aspects of the game at the current
+ * state.
+ * mode keeps track of game mode
+ * p___Hero keeps track of which hero each player is using
+ * p___HP is the current hitpoints of player
+ * p___Hand is the list of cards in a player's hand
+ * p___Board is the cards currently on each side of the board
+ * p___Deck is each respective player's current deck
+ * hUsed is whether or not the player used their hero power that turn
+ * p___Mana is the mana for each player
+ * turn is 0 or 1 depending on who's player's turn it is
+ *)
+type board = {
+	mode      : mode;
+	pOneHero  : hero;
+	pTwoHero  : hero;
+	pOneHP    : int ref;
+	pTwoHP    : int ref;
+	pOneHand  : card list ref;
+	pTwoHand  : card list ref;	
+	pOneBoard : card list ref;
+	pTwoBoard : card list ref;
+	pOneDeck  : deck ref;
+	pTwoDeck  : deck ref;
+	hUsed     : bool ref;
+	pOneMana  : mana ref;
+	pTwoMana  : mana ref;
+	turn      : int ref;
+}
+
+(* [makeBoard] takes in the two hero deck tuples and creates the 
+ * starting board state
+ *)
+val makeBoard      : (hero * deck) -> (hero * deck) -> mode -> board
+
+(* [actualGame] acts as a main funciton that starts the command parsing
+ *)
+val actualGame     : Unit -> Unit
---
+++ deck.mli
@@ -0,0 +1,82 @@
+
+(** Type of a card
+ *  Spell is a spell, while Protoss,Zerg, and Terran cards are minions
+ *)
+type ctyp =
+    | Protoss
+    | Zerg
+    | Terran
+    | Spell
+
+(** etype is the type of the effect where None means no 
+ * deathrattle or battlecry. Battlecry effects trigger upon
+ * being played and Deathrattle triggers upon death of card
+ *)
+type etype = 
+    | None
+    (* effect happens when card is played *)
+    | BattleCry   of etype
+    (* effect happens when card leaves board *)
+    | DeathRattle of etype
+    (* draw a certain number of cards where bool represents self or opponent *)
+    | Draw        of bool * int
+    (* buff all friendly cards hp and atk respectively *)
+    | AoeF        of int * int
+    (* buff all enemy cards hp and atk respectively *)
+    | AoeE        of int * int
+    (* buff all cards on the board hp and atk respectively *)
+    | AoeA        of int * int
+    (* buff all cards of type ctype on your side of board *)
+    | BType       of ctyp * int * int
+    (* buff one select minion *)
+    | BOne        of int * int
+
+(** The effect of a given card can have different types
+ *
+ * 
+ *)
+type effect = {
+    description : string;
+    efftype     : etype;
+
+}
+
+(** Type of a card
+ *  Spell is a spell, while Protoss,Zerg, and Terran cards are minions
+ *)
+type ctyp =
+    | Protoss
+    | Zerg
+    | Terran
+    | Spell
+
+(** A record representing all the information about a card.
+ *  It contains the anme and basic stats of the card; hp, attack, effect
+ *  The stealth and taunt fields are bools of whether a card has
+ *  stealth or taunt at any given time.
+ *  The effect
+ *)
+type card = {
+    name  : string;
+    hp    : int ref;
+    atk   : int ref;
+    effect: effect;
+    stealh: bool;
+    taunt : bool;
+    ctype : ctyp;
+    cost  : int;
+}
+
+(* card library with all possible cards *)
+type clist  = card array;
+(* a list of cards to simulate the deck *)
+type deck = card list
+
+(* [draw] [deck] returns a tuple with the rest of the deck and the top card
+ * removed.
+ *)
+val draw : deck   -> deck * card
+
+(* [shuffle] [deck] shuffles the deck and returns a randomized card list
+ *)
+val shuffle: deck -> deck
